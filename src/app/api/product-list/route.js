9a0c8919174667d78ccd2546505d6b1aae71737e
tanvirpiash <tanvir9290@gmail.com>
2024-04-23 09:29:09 +0600

    remove of routes try catch block which causing production build error on vercel

--- src/app/api/product-list/route.js
+++ src/app/api/product-list/route.js
@@ -1,13 +1,14 @@
 import { promises as fs } from 'fs';
 export async function GET() {
-   try {
-      const productList = await fs.readFile(
-         process.cwd() + '/public/products.json',
-         'utf8'
-      );
-      const products = JSON.parse(productList);
-      return Response.json(products);
-   } catch (error) {
-      return Response.error();
-   }
+   // try {
+
+   // } catch (error) {
+   //    return Response.error();
+   // }
+   const productList = await fs.readFile(
+      process.cwd() + '/public/products.json',
+      'utf8'
+   );
+   const products = JSON.parse(productList);
+   return Response.json(products);
 }
--- src/app/api/product-single/route.js
+++ src/app/api/product-single/route.js
@@ -1,17 +1,18 @@
 import { promises as fs } from 'fs';
 export async function GET(request) {
-   try {
-      const url = new URL(request.url);
-      const searchParams = new URLSearchParams(url.searchParams);
-      const id = searchParams.get('id');
-      const productList = await fs.readFile(
-         process.cwd() + '/public/products.json',
-         'utf8'
-      );
-      const products = JSON.parse(productList);
-      const productSingle = products.find((item) => item.id == id);
-      return Response.json(productSingle);
-   } catch (error) {
-      return Response.error();
-   }
+   // try {
+
+   // } catch (error) {
+   //    return Response.error();
+   // }
+   const url = new URL(request.url);
+   const searchParams = new URLSearchParams(url.searchParams);
+   const id = searchParams.get('id');
+   const productList = await fs.readFile(
+      process.cwd() + '/public/products.json',
+      'utf8'
+   );
+   const products = JSON.parse(productList);
+   const productSingle = products.find((item) => item.id == id);
+   return Response.json(productSingle);
 }
